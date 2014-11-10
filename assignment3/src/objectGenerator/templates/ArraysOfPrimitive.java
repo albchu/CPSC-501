8c833c3e58e735ca01f423820beb4fb72cb1f3d1
Albert Chu <albertchu539@gmail.com>
2014-11-10 15:59:45 -0700

    Added template classes to object generator

---
+++ assignment3/src/objectGenerator/templates/ArraysOfPrimitive.java
@@ -0,0 +1,13 @@
+package objectGenerator.templates;
+
+public class ArraysOfPrimitive {
+	public int[] arrayInt;
+
+	public int[] getArrayInt() {
+		return arrayInt;
+	}
+
+	public void setArrayInt(int[] arrayInt) {
+		this.arrayInt = arrayInt;
+	}
+}
---
+++ assignment3/src/objectGenerator/templates/SimpleChars.java
@@ -0,0 +1,32 @@
+package objectGenerator.templates;
+
+public class SimpleChars {
+	private char varChar1;
+	private char varChar2;
+	private char varChar3;
+	private char varChar4;
+	public char getVarChar1() {
+		return varChar1;
+	}
+	public void setVarChar1(char varChar1) {
+		this.varChar1 = varChar1;
+	}
+	public char getVarChar2() {
+		return varChar2;
+	}
+	public void setVarChar2(char varChar2) {
+		this.varChar2 = varChar2;
+	}
+	public char getVarChar3() {
+		return varChar3;
+	}
+	public void setVarChar3(char varChar3) {
+		this.varChar3 = varChar3;
+	}
+	public char getVarChar4() {
+		return varChar4;
+	}
+	public void setVarChar4(char varChar4) {
+		this.varChar4 = varChar4;
+	}
+}
