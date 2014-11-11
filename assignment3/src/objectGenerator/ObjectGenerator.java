31b3598f5db6eef633f42913337dfa167ccdf59d
Albert Chu <albertchu539@gmail.com>
2014-11-11 01:10:54 -0700

    Successfully can update field in primitive cases

--- assignment3/src/objectGenerator/ObjectGenerator.java
+++ assignment3/src/objectGenerator/ObjectGenerator.java
@@ -6,112 +6,195 @@ import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
-import objectGenerator.templates.SimpleInts;
+import objectGenerator.templates.*;
 import utilities.TextDisplay;
 import utilities.Utilities;
 
 public class ObjectGenerator {
 	private List<String> mainMenuText;
-	private List<String> RetryText;
-	private List<String> SimplePrimitiveText;
+	private List<String> YesNoMenu;
+	private List<String> ObjectMenu;
+	private List<String> ObjectGenerationMenu;
 	private Scanner userInput = new Scanner(System.in);
 	private List<Object> objList;
 
 	public ObjectGenerator() {
-		mainMenuText = Utilities.readToList("src/objectGenerator/assets/MainMenuText.txt");
-		RetryText = Utilities.readToList("src/objectGenerator/assets/MainMenuText.txt");
-//		SimplePrimitiveText = Utilities.readToList("src/objectGenerator/assets/MainMenuText.txt");
+		mainMenuText = Utilities
+				.readToList("src/objectGenerator/assets/MainMenuText.txt");
+		YesNoMenu = Utilities
+				.readToList("src/objectGenerator/assets/YesNo.txt");
+		ObjectMenu = Utilities
+				.readToList("src/objectGenerator/assets/ObjectMenu.txt");
+		ObjectGenerationMenu = Utilities
+				.readToList("src/objectGenerator/assets/ObjectGenerationMenu.txt");
+		// SimplePrimitiveText =
+		// Utilities.readToList("src/objectGenerator/assets/MainMenuText.txt");
 		objList = new ArrayList<Object>();
 	}
 
-	public void mainMenu() {
-		switch (menuSelect(mainMenuText)) {
+	public void objGeneratorMenu() {
+		switch (menuSelect(ObjectGenerationMenu)) {
 		case (1):
 			objList.add(createSimplePrimitive());
 			break;
 		case (2):
-			
+			objList.add(createSimpleObject());
+			break;
 		case (3):
 
 		case (4):
 
 		case (5):
 
+		case (6):
+			mainMenu();
 		}
-		retry();
 	}
 	
-	public Object createSimplePrimitive()
-	{
+	public void mainMenu() {
+		switch (menuSelect(mainMenuText)) {
+		case (1):
+			objGeneratorMenu();
+		break;
+		case (2):
+			TextDisplay.display("PLACEHOLDER FOR VIEW OBJS");
+		break;
+		case (3):
+			TextDisplay.display("PLACEHOLDER FOR SERIALIZE AND SEND");
+		break;
+			
+		case (4):
+			TextDisplay.display("Now terminating program");
+			System.exit(0);
+		}
+		mainMenu();
+	}
+	
+	public Object createSimpleObject() {
+		Object obj = new SimpleObjects();
+		setFields(obj);
+		return obj;
+	}
+
+	public Object createSimplePrimitive() {
 		Object obj = new SimpleInts();
 		setFields(obj);
 		return obj;
 	}
 
-	public void setFields(Object obj)
-	{
+	public void setFields(Object obj) {
 		TextDisplay.display("Field names");
 		List<String> fieldList = getFieldList(obj);
-		fieldList.add((fieldList.size()) + ") Exit");
-		int selection = menuSelect(fieldList);
-		if(selection > 0 && selection <= fieldList.size())
-			setField(obj, selection - 1);
+		fieldList.add((fieldList.size() + 1) + ") Exit");
+		int selection = -1;
+		while (selection != (fieldList.size() + 1)) {
+			selection = menuSelect(fieldList);
+			if (selection > 0 && selection <= fieldList.size() - 1)
+				setField(obj, selection - 1);
+			else
+				break;
+		}
+		objGeneratorMenu();
 	}
-	
+
 	/**
 	 * Allows the user to set a specific field
+	 * 
 	 * @param obj
 	 * @param fieldIndex
 	 */
-	public void setField(Object obj, int fieldIndex)
-	{
+	public void setField(Object obj, int fieldIndex) {
 		Field field = obj.getClass().getDeclaredFields()[fieldIndex];
+		Class<?> fieldType = field.getType();
 		TextDisplay.display("You have selected field: " + field.getName());
+		TextDisplay.display("The type is: " + fieldType.getName());
+		int input = 0;
+		if (fieldType.equals(int.class)) {
+			TextDisplay.display("Please enter an int to set the field:");
+			try {
+				input = userInput.nextInt();
+			} catch (InputMismatchException e) {
+				TextDisplay.display("Invalid selection, try again.");
+				userInput.next();
+				setField(obj, fieldIndex);
+			}
+			field.setAccessible(true);
+			
+			try {
+				field.set(obj, input);
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
+			TextDisplay.display("Successfully set field: " + field.getName() + " to " + input);
+		} else if (fieldType.equals(Object.class))
+		{
+			TextDisplay.display("Please select what object to choose:");
+			switch (menuSelect(ObjectMenu)) {
+			case (1):
+				mainMenu();
+			break;
+			case (2):
+				menuSelect(createdObjects());
+			break;
+			}
+		}
 	}
-	
-	public  List<String> getFieldList(Object obj)
+
+	/**
+	 * Returns a list of all the objects created
+	 * @return
+	 */
+	public List<String> createdObjects()
 	{
+		List<String> createdObjectClassNames = new ArrayList<String>();
+		for(Object obj : objList)
+			createdObjectClassNames.add(obj.toString());
+		return createdObjectClassNames;
+	}
+		
+	public List<String> getFieldList(Object obj) {
 		List<String> fieldList = new ArrayList<String>();
 		int i = 0;
 		for (Field field : obj.getClass().getDeclaredFields())
 			fieldList.add((++i) + ") " + field.getName());
 		return fieldList;
 	}
-	
-	public void retry()
-	{
-		TextDisplay.display("Would you like to create another object?");
-		switch (menuSelect(RetryText)) {
-		case (1):
-			mainMenu();
-		break;
-		case (2):
-			serialize();
-		break;
-		}
-	}
-	public void serialize()
-	{
+
+//	public void retry() {
+//		TextDisplay.display("Would you like to create another object?");
+//		switch (menuSelect(RetryText)) {
+//		case (1):
+//			objGeneratorMenu();
+//			break;
+//		case (2):
+//			serialize();
+//			break;
+//		}
+//	}
+
+	public void serialize() {
 		TextDisplay.display("Would you like to serialize objects?");
-		switch (menuSelect(RetryText)) {
+		switch (menuSelect(YesNoMenu)) {
 		case (1):
 			serializeList(objList);
-		break;
+			break;
 		case (2):
 			System.exit(0);
-		break;
+			break;
 		}
 	}
-	
+
 	/**
 	 * Serializes all elements in the parameter list
+	 * 
 	 * @param list
 	 */
-	public void serializeList(List<Object> list)
-	{
+	public void serializeList(List<Object> list) {
 		TextDisplay.display("This isnt implemented");
 	}
-	
+
 	/**
 	 * Print a menu and do not return until valid option is selected
 	 * 
@@ -132,6 +215,7 @@ public class ObjectGenerator {
 			}
 		} while (input < 0 || input > menuList.size());
 		TextDisplay.display("You have selected: " + input);
+		TextDisplay.display(TextDisplay.repeatChar("#", 50));
 		return input;
 	}
 
