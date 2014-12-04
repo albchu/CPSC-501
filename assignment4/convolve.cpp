2d47f44681004f0f1c6f5440641a14acfaf09fb9
Albert Chu <albertchu539@gmail.com>
2014-12-04 05:45:41 -0700

    Tried some crazy fft implementation, did not work, trying something nelse

--- assignment4/convolve.cpp
+++ assignment4/convolve.cpp
@@ -2,16 +2,28 @@
 #include <stdio.h>
 #include <sndfile.h>
 #include <sndfile.hh>
-//#include <stdlib.h>
-#include <time.h> 
+#include <time.h>
+#include <algorithm>
 #include <iostream>
 using namespace std;
 
+#define SWAP(a,b)  tempr=(a);(a)=(b);(b)=tempr
+
 /*  Function prototypes  */
-void convolve(float x[], int N, float h[], int M, float y[], int P);
+void convolve(double input_signal[], int input_size, double impulse_response[], int impulse_size, double output_signal[], int output_size, int pow2);
 void print_vector(char *title, float x[], int N);
 
-int read_wav(const char *file_name, float *& buf)
+
+void FFTScaling(double *& signal, int N)
+{
+	int k;
+	int i;
+	for (k = 0, i = 0; k < N; k++, i += 2) {
+		signal[i] /= (float)N;
+		signal[i + 1] /= (float)N;
+	}
+}
+int read_wav(const char *file_name, double *& buf)
 {
 	SNDFILE *sf;
 	SF_INFO info;
@@ -42,17 +54,16 @@ int read_wav(const char *file_name, float *& buf)
 	printf("num_items=%d\n", num_items);
 	/* Allocate space for the data to be read, then read it. */
 	//buf = (int *)malloc(num_items*sizeof(int));
-	buf = new float[info.channels * info.frames];
-	num = sf_read_float(sf, buf, num_items);
+	buf = new double[info.channels * info.frames];
+	num = sf_read_double(sf, buf, num_items);
 	sf_close(sf);
 	printf("Read %d items\n", num);
 	return num;
 }
 
-int write_wav(const char *file_name, float *& buf, int num)
+int write_wav(const char *file_name, double *& buf, int num)
 {
 	const int format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
-	//  const int format=SF_FORMAT_WAV | SF_FORMAT_FLOAT;
 	const int channels = 1;
 	const int sampleRate = 48000;
 
@@ -62,6 +73,104 @@ int write_wav(const char *file_name, float *& buf, int num)
 	return outfile.write(buf, num);
 }
 
+//  The four1 FFT from Numerical Recipes in C,
+//  p. 507 - 508.
+//  Note:  changed float data types to double.
+//  nn must be a power of 2, and use +1 for
+//  isign for an FFT, and -1 for the Inverse FFT.
+//  The data is complex, so the array size must be
+//  nn*2. This code assumes the array starts
+//  at index 1, not 0, so subtract 1 when
+//  calling the routine (see main() below).
+
+void four1(double data[], int nn, int isign)
+{
+	unsigned long n, mmax, m, j, istep, i;
+	double wtemp, wr, wpr, wpi, wi, theta;
+	double tempr, tempi;
+
+	n = nn << 1;
+	j = 1;
+
+	for (i = 1; i < n; i += 2) {
+		if (j > i) {
+			SWAP(data[j], data[i]);
+			SWAP(data[j + 1], data[i + 1]);
+		}
+		m = nn;
+		while (m >= 2 && j > m) {
+			j -= m;
+			m >>= 1;
+		}
+		j += m;
+	}
+
+	mmax = 2;
+	while (n > mmax) {
+		istep = mmax << 1;
+		theta = isign * (6.28318530717959 / mmax);
+		wtemp = sin(0.5 * theta);
+		wpr = -2.0 * wtemp * wtemp;
+		wpi = sin(theta);
+		wr = 1.0;
+		wi = 0.0;
+		for (m = 1; m < mmax; m += 2) {
+			for (i = m; i <= n; i += istep) {
+				j = i + mmax;
+				tempr = wr * data[j] - wi * data[j + 1];
+				tempi = wr * data[j + 1] + wi * data[j];
+				data[j] = data[i] - tempr;
+				data[j + 1] = data[i + 1] - tempi;
+				data[i] += tempr;
+				data[i + 1] += tempi;
+			}
+			wr = (wtemp = wr) * wpr - wi * wpi + wr;
+			wi = wi * wpr + wtemp * wpi + wi;
+		}
+		mmax = istep;
+	}
+}
+//
+//// Takes the results from a DFT or FFT, and
+//// calculates and displays the amplitudes of
+//// the harmonics.
+//
+//void postProcessComplex(double *& x, int N)
+//{
+//	int i, k, j;
+//	double *amplitude, *result;
+//
+//	// Allocate temporary arrays
+//	amplitude = (double *)calloc(N, sizeof(double));
+//	result = (double *)calloc(N, sizeof(double));
+//
+//	// Calculate amplitude
+//	for (k = 0, i = 0; k < N; k++, i += 2) {
+//		// Scale results by N
+//		double real = x[i] / (double)N;
+//		double imag = x[i + 1] / (double)N;
+//		// Calculate amplitude
+//		amplitude[k] = sqrt(real * real + imag * imag);
+//	}
+//
+//	// Combine amplitudes of positive and negative frequencies
+//	result[0] = amplitude[0];
+//	result[N / 2] = amplitude[N / 2];
+//	for (k = 1, j = N - 1; k < N / 2; k++, j--)
+//		result[k] = amplitude[k] + amplitude[j];
+//
+//
+//	//// Print out final result
+//	//printf("Harmonic \tAmplitude\n");
+//	//printf("DC \t\t%.6f\n", result[0]);
+//	//for (k = 1; k <= N / 2; k++)
+//	//	printf("%-d \t\t%.6f\n", k, result[k]);
+//	//printf("\n");
+//
+//	// Free up memory used for arrays
+//	free(amplitude);
+//	free(result);
+//}
 /*****************************************************************************
 *
 *    Function:     main
@@ -85,7 +194,7 @@ int main(int argc, char* argv[])
 	cout << "Input Response file: " << input_response_file << endl;
 	cout << "Output file: " << output_file << endl;
 
-	float *input_signal, *impulse_response;
+	double *input_signal, *impulse_response;
 	int input_size = read_wav(input_file, input_signal);
 	int impulse_size = read_wav(input_response_file, impulse_response);
 
@@ -93,12 +202,21 @@ int main(int argc, char* argv[])
 	//print_vector("Original input signal", buf, num);
 
 	/*  Set the expected size of the output signal  */
+	int max_size = max(input_size, impulse_size);
+
+	int pow2 = (int)log2(max_size) + 1;
+	pow2 = pow(2, pow2);
+
 	int output_size = input_size + impulse_size - 1;
-	float * output_signal = new float[output_size];
+	//double * output_signal = new double[2 * output_size];
+	double * output_signal = new double[output_size];
+
+	//outputSignal = new float[outputSignalSize];
+
 	/*  Do the convolution, and print the output signal  */
 	clock_t t;
 	t = clock();
-	convolve(input_signal, input_size, impulse_response, impulse_size, output_signal, output_size);
+	convolve(input_signal, input_size, impulse_response, impulse_size, output_signal, output_size, pow2);
 	t = clock() - t;
 	cout << "Total amount of time spent convoluting: " << ((float)t) / CLOCKS_PER_SEC << " seconds" << endl;
 	write_wav(output_file, output_signal, output_size);
@@ -125,33 +243,53 @@ int main(int argc, char* argv[])
 *                       equal N + M - 1
 *
 *****************************************************************************/
-void convolve(float x[], int N, float h[], int M, float y[], int P)
+void convolve(double input_signal[], int input_size, double impulse_response[], int impulse_size, double output_signal[], int output_size, int pow2)
 {
-  int n, m;
 
-  /*  Make sure the output buffer is the right size: P = N + M - 1  */
-  if (P != (N + M - 1)) {
-    printf("Output signal vector is the wrong size\n");
-    printf("It is %-d, but should be %-d\n", P, (N + M - 1));
-    printf("Aborting convolution\n");
-    return;
-  }
+	double *hComplex = new double[2 * pow2];
+	double *xComplex = new double[2 * pow2];
+	double *yComplex = new double[2 * pow2];
 
-  /*  Clear the output buffer y[] to all zero values  */  
-  cout << "Clearing the output buffer to all zero values...";
-  for (n = 0; n < P; n++)
-    y[n] = 0.0;
-  cout << "Completed" << endl;
-  /*  Do the convolution  */
-  /*  Outer loop:  process each input value x[n] in turn  */
-  cout << "Convolution loop: " << endl;
-  for (n = 0; n < N; n++) {
-	  cout << ((float)n)/N * 100 << "%" << "\r";
-    /*  Inner loop:  process x[n] with each sample of h[]  */
-    for (m = 0; m < M; m++)
-      y[n+m] += x[n] * h[m];
-  }
-  cout << endl;
+
+	int i = 0;
+
+	//set hComplex with 0's
+	for (i = 0; i < 2 * pow2; i++) {
+		hComplex[i] = 0.0;
+	}
+
+	//set xComplex with 0's
+	for (i = 0; i < 2 * pow2; i++) {
+		xComplex[i] = 0.0;
+	}
+
+	//padding the complex number with 0 and the real number with original value for h
+	for (i = 0; i < impulse_size; i++) {
+		hComplex[2 * i] = impulse_response[i];
+	}
+
+	//padding the complex number with 0 and the real number with original value for x
+	for (i = 0; i < input_size; i++) {
+		xComplex[2 * i] = input_signal[i];
+	}
+
+
+	four1(hComplex, pow2, 1);
+	four1(xComplex, pow2, 1);
+
+	for (i = 0; i < output_size; i++) {
+		yComplex[i * 2] = xComplex[i] * hComplex[i] - xComplex[i + 1] * hComplex[i + 1];
+		yComplex[i * 2 + 1] = xComplex[i + 1] * hComplex[i] + xComplex[i] * hComplex[i + 1];
+	}
+
+	four1(yComplex - 1, pow2, -1);
+	//FFTScaling(yComplex, pow2);
+
+	// removing padding
+	for (i = 0; i < output_size; i++) {
+		output_signal[i] = yComplex[i * 2];
+	}
+	//postProcessComplex(output_signal, output_size);
 }
 
 
