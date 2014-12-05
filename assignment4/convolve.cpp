d50105264f622b072139288dbdcec5a12d7273be
Albert Chu <albertchu539@gmail.com>
2014-12-05 03:24:44 -0700

    FFT basic completed

--- assignment4/convolve.cpp
+++ assignment4/convolve.cpp
@@ -2,19 +2,28 @@
 #include <stdio.h>
 #include <sndfile.h>
 #include <sndfile.hh>
-#include <time.h> 
-#include <stdlib.h>
+#include <time.h>
+#include <algorithm>
 #include <iostream>
-
 using namespace std;
-
+#define LOG_TWO		0.69314718	
 #define SWAP(a,b)  tempr=(a);(a)=(b);(b)=tempr
 
 /*  Function prototypes  */
-void convolve(float x[], int N, float h[], int M, float y[], int P);
+void convolve(double input_signal[], int input_size, double impulse_response[], int impulse_size, double *& output_signal, int output_size);
 void print_vector(char *title, float x[], int N);
 
-int read_wav(const char *file_name, float *& buf)
+
+void FFTScaling(double *& signal, int N)
+{
+	int k;
+	int i;
+	for (k = 0, i = 0; k < N; k++, i += 2) {
+		signal[i] /= (double)N;
+		signal[i + 1] /= (double)N;
+	}
+}
+int read_wav(const char *file_name, double *& buf)
 {
 	SNDFILE *sf;
 	SF_INFO info;
@@ -45,26 +54,18 @@ int read_wav(const char *file_name, float *& buf)
 	printf("num_items=%d\n", num_items);
 	/* Allocate space for the data to be read, then read it. */
 	//buf = (int *)malloc(num_items*sizeof(int));
-	buf = new float[info.channels * info.frames];
-	num = sf_read_float(sf, buf, num_items);
+	buf = new double[info.channels * info.frames];
+	num = sf_read_double(sf, buf, num_items);
 	sf_close(sf);
 	printf("Read %d items\n", num);
 	return num;
 }
 
-int write_wav(const char *file_name, float *& buf, int num)
+int write_wav(const char *file_name, double *& buf, int num)
 {
 	const int format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
 	const int channels = 1;
-	const int sampleRate = 48000;
-
-	SndfileHandle outfile(file_name, SFM_WRITE, format, channels, sampleRate);
-	if (!outfile) return -1;
-
-	const int format = SF_FORMAT_WAV | SF_FORMAT_PCM_16;
-	//  const int format=SF_FORMAT_WAV | SF_FORMAT_FLOAT;
-	const int channels = 1;
-	const int sampleRate = 48000;
+	const int sampleRate = 44100;
 
 	SndfileHandle outfile(file_name, SFM_WRITE, format, channels, sampleRate);
 	if (!outfile) return -1;
@@ -130,47 +131,6 @@ void four1(double data[], int nn, int isign)
 	}
 }
 
-
-// Takes the results from a DFT or FFT, and
-// calculates and displays the amplitudes of
-// the harmonics.
-
-void postProcessComplex(double x[], int N)
-{
-	int i, k, j;
-	double *amplitude, *result;
-
-	// Allocate temporary arrays
-	amplitude = (double *)calloc(N, sizeof(double));
-	result = (double *)calloc(N, sizeof(double));
-
-	// Calculate amplitude
-	for (k = 0, i = 0; k < N; k++, i += 2) {
-		// Scale results by N
-		double real = x[i] / (double)N;
-		double imag = x[i + 1] / (double)N;
-		// Calculate amplitude
-		amplitude[k] = sqrt(real * real + imag * imag);
-	}
-
-	// Combine amplitudes of positive and negative frequencies
-	result[0] = amplitude[0];
-	result[N / 2] = amplitude[N / 2];
-	for (k = 1, j = N - 1; k < N / 2; k++, j--)
-		result[k] = amplitude[k] + amplitude[j];
-
-
-	// Print out final result
-	printf("Harmonic \tAmplitude\n");
-	printf("DC \t\t%.6f\n", result[0]);
-	for (k = 1; k <= N / 2; k++)
-		printf("%-d \t\t%.6f\n", k, result[k]);
-	printf("\n");
-
-	// Free up memory used for arrays
-	free(amplitude);
-	free(result);
-}
 /*****************************************************************************
 *
 *    Function:     main
@@ -194,45 +154,21 @@ int main(int argc, char* argv[])
 	cout << "Input Response file: " << input_response_file << endl;
 	cout << "Output file: " << output_file << endl;
 
-	float *input_signal, *impulse_response;
+	double *input_signal, *impulse_response;
 	int input_size = read_wav(input_file, input_signal);
 	int impulse_size = read_wav(input_response_file, impulse_response);
 
-	/*  Print out the input signal to the screen  */
-	//print_vector("Original input signal", buf, num);
-	
-	// Get the number of segments necessary to split the input signal into
-	int num_segments = (input_size + impulse_size - 1) / impulse_size;	// Get the division ceiling
+	int output_size = input_size + impulse_size - 1;
 
-	int remaining_input_size = input_size;
-	for (int i = 0; i < num_segments; i++)
-	{
-		int segment_size;
-		if (remaining_input_size < impulse_size)	// If there are less signals than impulse size left to parse, we should just use that data for the segment size
-			segment_size = remaining_input_size;
-		else
-		{
-			segment_size = impulse_size - 1;
-			remaining_input_size -= segment_size;
-		}
-		double *segment = new double[segment_size * 2];		// Times 2 for padding
-	}
-		//int num_segments = input_size /
+	double * output_signal = new double[output_size];
 
-
-
-
-
-	///*  Set the expected size of the output signal  */
-	//int output_size = input_size + impulse_size - 1;
-	//float * output_signal = new float[output_size];
-	///*  Do the convolution, and print the output signal  */
-	//clock_t t;
-	//t = clock();
-	//convolve(input_signal, input_size, impulse_response, impulse_size, output_signal, output_size);
-	//t = clock() - t;
-	//cout << "Total amount of time spent convoluting: " << ((float)t) / CLOCKS_PER_SEC << " seconds" << endl;
-	//write_wav(output_file, output_signal, output_size);
+	/*  Do the convolution, and print the output signal  */
+	clock_t t;
+	t = clock();
+	convolve(input_signal, input_size, impulse_response, impulse_size, output_signal, output_size);
+	t = clock() - t;
+	cout << "Total amount of time spent convoluting: " << ((float)t) / CLOCKS_PER_SEC << " seconds" << endl;
+	write_wav(output_file, output_signal, output_size);
 
 	/*  End of program  */
 	return 0;
@@ -256,57 +192,50 @@ int main(int argc, char* argv[])
 *                       equal N + M - 1
 *
 *****************************************************************************/
-void convolve(float x[], int N, float h[], int M, float y[], int P)
+void convolve(double input_signal[], int input_size, double impulse_response[], int impulse_size, double *& output_signal, int output_size)
 {
-  int n, m;
+	int pow2 = log2(input_size) + 1;
+	pow2 = pow(2, pow2);
+	int double_pow2 = 2 * pow2;
 
-  /*  Make sure the output buffer is the right size: P = N + M - 1  */
-  if (P != (N + M - 1)) {
-    printf("Output signal vector is the wrong size\n");
-    printf("It is %-d, but should be %-d\n", P, (N + M - 1));
-    printf("Aborting convolution\n");
-    return;
-  }
+	double *x_complex = new double[double_pow2];
+	double *h_complex = new double[double_pow2];
+	double *y_complex = new double[double_pow2];
 
-  /*  Clear the output buffer y[] to all zero values  */  
-  cout << "Clearing the output buffer to all zero values...";
-  for (n = 0; n < P; n++)
-    y[n] = 0.0;
-  cout << "Completed" << endl;
-  /*  Do the convolution  */
-  /*  Outer loop:  process each input value x[n] in turn  */
-  cout << "Convolution loop: " << endl;
-  for (n = 0; n < N; n++) {
-	  printf("Percent Completed: %.2f\r", ((float)n) / N * 100);
-	  //cout << ((float)n)/N * 100 << "%" << "\r";
-    /*  Inner loop:  process x[n] with each sample of h[]  */
-    for (m = 0; m < M; m++)
-      y[n+m] += x[n] * h[m];
-  }
-  cout << endl;
+	//zero pad arrays
+	for (int i = 0; i < double_pow2; i++)
+		x_complex[i] = 0.0;
+	for (int i = 0; i < double_pow2; i++)
+		h_complex[i] = 0.0;
+	for (int i = 0; i < double_pow2; i++)
+		y_complex[i] = 0.0;
+
+	// Load data into x_complex to be used in fft
+	for (int i = 0; i < input_size; i++)
+		x_complex[2 * i] = input_signal[i];
+
+	// Load data into h_complex to be used in fft
+	for (int i = 0; i < impulse_size; i++)
+		h_complex[2 * i] = impulse_response[i];
+
+	// FFT
+	four1(h_complex - 1, pow2, 1);
+	four1(x_complex - 1, pow2, 1);
+
+	// Convolution
+	for (int i = 0; i < double_pow2; i += 2)
+	{
+		y_complex[i] = x_complex[i] * h_complex[i] - x_complex[i + 1] * h_complex[i + 1];
+		y_complex[i + 1] = x_complex[i + 1] * h_complex[i] + x_complex[i] * h_complex[i + 1];
+	}
+
+	// Inverse FFT
+	four1(y_complex - 1, pow2, -1);
+
+	// Scale real parts of the output
+	for (int i = 0; i < output_size; i++)
+	{
+		output_signal[i] = y_complex[i * 2] / (double_pow2 * 2);
+	}
 }
 
-
-
-/*****************************************************************************
-*
-*    Function:     print_vector
-*
-*    Description:  Prints the vector out to the screen
-*
-*    Parameters:   title is a string naming the vector
-*                  x[] is the vector to be printed out
-*                  N is the number of samples in the vector x[]
-*
-*****************************************************************************/
-
-void print_vector(char *title, float x[], int N)
-{
-  int i;
-
-  printf("\n%s\n", title);
-  printf("Vector size:  %-d\n", N);
-  printf("Sample Number \tSample Value\n");
-  for (i = 0; i < N; i++)
-    printf("%-d\t\t%f\n", i, x[i]);
-}
